658a534a53611709b68eadeb6c778c41e871b42a
pix <tpxsky@163.com>
2021-03-18 13:37:42 +0800

    modify keybinds

--- config.h
+++ config.h
@@ -64,7 +64,7 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *screenshotcmd[] = { "flameshot", "gui", NULL };
-static const char *trayercmd[]  = { "/bin/bash", "$HOME/dev/source-code/shell/t-toggle.sh" };
+static const char *trayercmd[]  = { "$HOME/dev/source-code/shell/t-toggle.sh" , NULL};
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
